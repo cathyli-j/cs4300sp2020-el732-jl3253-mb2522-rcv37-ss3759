519b77cf80f7b15e25e085e7b54a49fe83d3de1c
Cathy <jl3253@cornell.edu>
2020-04-06 15:41:55 -0400

    tutorial

--- app/irsystem/controllers/__init__.py
+++ app/irsystem/controllers/__init__.py
@@ -24,5 +24,3 @@ from app import socketio
 from app.irsystem import irsystem 
 
 # Import module models
-from app.accounts.models.user import *
-from app.accounts.models.session import *
--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,10 +2,10 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Better Yelp Recommendations"
+net_id = "Emanuele Lusso: el732, Cathy Li: jl3253, Matias Blake: mb2522, Robert Villaluz: rcv37, Saaqeb Siddiqi: ss3759"
 
-@irsystem.route('/', methods=['GET'])
+@irsystem.route('/page1', methods=['GET'])
 def search():
 	query = request.args.get('search')
 	if not query:
@@ -13,7 +13,7 @@ def search():
 		output_message = ''
 	else:
 		output_message = "Your search: " + query
-		data = range(5)
+		data = 'hello world'
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
