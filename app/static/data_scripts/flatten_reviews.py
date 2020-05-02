f01d2d01aea179bb7a6e1818a9d2fb3db715cd2c
Robert Villaluz <rcv37@cornell.edu>
2020-05-02 19:13:28 -0400

    Update restaurants list

--- app/static/data_scripts/flatten_reviews.py
+++ app/static/data_scripts/flatten_reviews.py
@@ -1,24 +1,49 @@
 import json
 import sys
-# TODO: We need to change this to the full scope of reviews we're interested in
-file_name = "../all_splits/reviews.json"
+import nltk
+from nltk.tokenize import word_tokenize 
+# Used: split -l 130000 ../yelp_academic_dataset_review.json review
+from os import listdir
+from os.path import isfile, join
+review_splits = [("../review_splits/" + f) for f in listdir("../review_splits") if isfile(join("../review_splits", f))]
+stop_words = {"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"}
+with open("../all_restaurants.json") as restaurants:
+    indexed_restaurants = [b['business_id'] for b in json.load(restaurants)]
+print(len(indexed_restaurants))
 
-with open(file_name) as json_file:
-    data = json.load(json_file)
-    flat_reviews = {}
-    errs = 0
-    good = 0
-    print("Flattening reviews")
-    for r in data:
-        try:
-            if r['business_id'] in flat_reviews:
-                flat_reviews[r['business_id']] += ' ' + r['text'].lower()
-            else:
-                flat_reviews[r['business_id']] = r['text'].lower()
-            good += 1
-        except:
-            #print("err with %s" % r)
-            errs += 1
+"""
+Returns a string with stop words removed
+"""
+def remove_stopwords(text):
+    review_text = text.lower()
+    review_tokens = word_tokenize(review_text) 
+    filtered_review_text = ""
+    for w in review_tokens: 
+        if w not in stop_words: 
+            filtered_review_text += ' ' + w
+    return filtered_review_text    
+
+print("Flattening and tokenizing reviews")
+flat_reviews = {}
+errs = 0
+good = 0
+for file_name in review_splits:
+    print("Loading " + file_name)
+    with open(file_name) as json_file:
+        print("Successfully opened " + file_name)
+        for line in json_file:
+            r = json.loads(line)
+            try:
+                if r['business_id'] in indexed_restaurants:
+                    if r['business_id'] in flat_reviews:
+                        flat_reviews[r['business_id']] += ' ' + remove_stopwords(r['text'])
+                    else:
+                        flat_reviews[r['business_id']] = remove_stopwords(r['text'])
+                    good += 1
+                    print(good)
+            except:
+                # print("err with %s" % r)
+                errs += 1
     with open('../all_splits/flat_reviews.json', 'w') as outfile:
         json.dump(flat_reviews, outfile)
 print("errs: %d" % errs)
--- app/static/data_scripts/get_restaurants.py
+++ app/static/data_scripts/get_restaurants.py
@@ -1,23 +1,27 @@
 import json
 import sys
 file_name = "../yelp_academic_dataset_business.json"
+cities_file = "../cities.json"
 
 category_list = ['Restaurants','Food','Sandwiches','Breakfast & Brunch','Chinese','Mexican','American (New)','Japanese','Coffee & Tea','Pizza','Cafes','American (Traditional)','Seafood','Italian','Sushi Bars','Burgers','Salad','Delis','Vietnamese','Noodles','Food Stands','Asian Fusion','Fast Food','Mediterranean','Thai','Food Trucks','Indian','Bakeries','Korean','Latin American','Dim Sum','Desserts','Barbeque','Ramen','Chicken Wings','Juice Bars & Smoothies','French','Soup','Diners','Sports Bars','Specialty Food','Grocery','Tacos','Cantonese','Hot Dogs','Gluten-Free','Halal','Middle Eastern','Tapas/Small Plates','Caterers','Pop-Up Restaurants','Bagels','Vegan','Vegetarian','Filipino','Lounges','Steakhouses','Bubble Tea','Chicken Shop','Creperies','Hot Pot','Comfort Food','Gastropubs','Greek','Salvadoran','Beer Bar','Hawaiian','Peruvian','Poke','Ice Cream & Frozen Yogurt','Street Vendors','Pakistani','Pubs','Donuts','Turkish','Izakaya','Southern','Tapas Bars','Himalayan/Nepalese','Hong Kong Style Cafe','Szechuan','Buffets','Cajun/Creole','Spanish','Brazilian','Cheesesteaks','Breweries','Burmese','Caribbean','German','Soul Food','Taiwanese','Japanese Curry']
 with open(file_name) as json_file:
-    data = json.load(json_file)
+    with open(cities_file) as cities_file:
+        cities_list = [city.split(', ')[0] for city in json.load(cities_file)]
     restaurants = []
     errs = 0
     good = 0
-
-    for d in data: 
+    for line in json_file:
+        d = json.loads(line) #https://stackoverflow.com/questions/21533894/how-to-read-line-delimited-json-from-large-file-line-by-line
         try:
-            if len(list(filter(lambda x: x in d['categories'], category_list))) > 0: 
+            if (d['categories'] is not None and len(list(filter(lambda x: x in d['categories'], category_list))) > 0 and 
+            d['city'] in cities_list):
                 restaurants.append(d)
                 good += 1
+                print(good)
         except Exception as e: 
             print(e)
             errs += 1
-    with open('all_restaurants.json', 'w') as outfile:
+    with open('../all_restaurants.json', 'w') as outfile:
         json.dump(restaurants, outfile)
     print("errs: %d" % errs)
     print("good: %d" % good)
